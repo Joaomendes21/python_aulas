43597e332c7e1016d25748ff1eaa4123a905d3c8
João Vítor mendes <88053372+Joaomendes21@users.noreply.github.com>
2025-06-06 10:17:47 -0300

    Add files via upload

---
+++ aula2-int-float.py
@@ -0,0 +1,12 @@
+print(11) #int = inteiro.
+print(-11) #int = inteiro.
+print(0) #int = inteiro.
+
+print(1.1, 10.11) #float = numeros com ponto.
+print(0.0, -1.5) #float = numeros com ponto.
+
+print(type ('10.0'), type(2.0), type(19)) #type e uma clase que mostra o tipo que estamos usando
+# exemplo - str - int - float - bool
+
+
+
---
+++ aula3-boolean.py
@@ -0,0 +1,3 @@
+print( 10 == 10) # sim = true = verdadeiro
+print(type(10 == 10)) #type bool
+print( 10 == 30) # não = false = falso
